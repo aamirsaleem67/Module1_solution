d2f4bb1e44b587240ea5b79d6c3001c4324b3966
Aamir Saleem <Aamir Saleem>
2017-01-02 19:01:39 +0500

    new web file

---
+++ app.js
@@ -0,0 +1,52 @@
+(function(){
+
+'use strict';
+    angular.module('LunchCheck',[])
+    .controller('LunchCheckController', myController);
+    function myController($scope)
+    {
+           $scope.items = "";
+           $scope.event = function()
+           {
+               calculateNumberOfItems($scope.items);
+           };
+
+           function calculateNumberOfItems(myitems)
+           {
+                  var splitarray= myitems.split(",");
+                  
+                  var myarray= checkEmptyString(splitarray);
+                  if(myarray.length == 0)
+                  {
+                     
+                             $scope.message="Please enter data first";
+                      
+                  }
+                  else if(myarray.length <= 3) {
+                      $scope.message="Enjoy!";
+                  }
+                  else{
+                     $scope.message = "Too much!";
+                  }
+
+           }
+           function checkEmptyString(splitarray)
+           {
+               var myarray=[];
+               for(var i =0; i< splitarray.length;i++)
+               {
+                   if(splitarray[i]=="" ||splitarray[i]==" ")
+                   {
+                    continue;
+                   }
+                   else{
+                       myarray.push(splitarray[i]);
+                   }
+               }  
+               
+              return myarray;
+           }
+
+    }
+  
+})();
